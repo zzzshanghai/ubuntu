e54fd0988c25ce5fb7a6193c9e9619ff2200fdb9
zzzshanghai <9117406+zzzshanghai@users.noreply.github.com>
2025-10-07 12:26:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
 usermod -aG sudo $SSH_USER
 echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
 echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
+ssserver -c /home/
 
 exec "$@"
