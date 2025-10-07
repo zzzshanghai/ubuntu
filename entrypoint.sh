414fe213e8f69926644a4128aad388a51c718fc6
zzzshanghai <9117406+zzzshanghai@users.noreply.github.com>
2025-10-07 10:59:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 #useradd -m -s /bin/bash $SSH_USER
-echo "root:$SSH_PASSWORD" | chpasswd
+#echo "root:$SSH_PASSWORD" | chpasswd
 #usermod -aG sudo $SSH_USER
 #echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
 #echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
