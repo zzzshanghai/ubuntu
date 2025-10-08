96eb034a03873c0b5bbbfcebb3d194e3604ddb51
zzzshanghai <9117406+zzzshanghai@users.noreply.github.com>
2025-10-08 18:47:04 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-useradd -m -s /bin/bash $SSH_USER
-echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
-usermod -aG sudo $SSH_USER
-echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
-echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
+#useradd -m -s /bin/bash $SSH_USER
+#echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
+#usermod -aG sudo $SSH_USER
+#echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
+#echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
 #hysteria-linux-amd64-avx server -c /home/config.yaml
 #ssserver -c /home/config.json
 
