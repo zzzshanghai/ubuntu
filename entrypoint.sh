4a29eef210a5547551d12e721a3b27ec4ecf624f
zzzshanghai <9117406+zzzshanghai@users.noreply.github.com>
2025-10-08 22:25:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@
 #echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
 #hysteria-linux-amd64-avx server -c /home/config.yaml
 #ssserver -c /home/config.json
+ttyd -p 8080 bash
 
 exec "$@"
